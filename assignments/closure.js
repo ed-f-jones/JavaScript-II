b19320d29ff12e5aaf1ac5829822d4bb578ed2c4
ed-f-jones <ed.f.jones@gmail.com>
2019-08-14 23:59:30 -0400

    javascript-II complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,12 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
+function popStar (firstName, lastName){
+  let comment = "I can't stand";
+  function makeWholeComment () {
+    return comment + firstName + "" + lastName;
+  }
+return makeWholeComment();
+}
+popStar("Cardi", "B");
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
